3e798553a048f25961d2b803cea8091c2b12708c
JINODK <36285359+JINODK@users.noreply.github.com>
2022-09-16 13:39:35 +0700

    Update configurations

--- src/configurations.h
+++ src/configurations.h
@@ -8,7 +8,7 @@
 
 /*****HARD-CODED CONFIGURATION*****/
 const char* ssid = "JINODK";
-const char* password = "jinodk2003";
+const char* password = "nevergonnaletyoudown";
 const char* hostname = "Mini-washing-machine";
 
 /*****HARDWARE PINS DEFINITION*****/
@@ -21,11 +21,10 @@ const char* hostname = "Mini-washing-machine";
 #define MOTOR 16    // D0
 
 /*****DEFAULT TIMER CONFIGURATION*****/
-// uint8_t washTime = 30;
-uint8_t spinTime = 20;//30;
+uint8_t spinTime = 20;
 uint8_t spinSpeed = 12; // out of 90
 uint8_t spinSpeed2 = 20; // drying spin speed
-uint8_t drainTime = 3;//10;
+uint8_t drainTime = 3;
 uint8_t pumpTime = 3;
-uint8_t dryBlow = 20;//200;
+uint8_t dryBlow = 20;
 // uint8_t stage = 1; // skip over setup stage
